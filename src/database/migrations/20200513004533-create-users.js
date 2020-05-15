36e742ce99bdcf3aa1f935e8b55165ebe2a896e6
Fabricio <fabricio.riff2@gmail.com>
2020-05-15 20:42:37 -0300

    relacionamentos muitos pra muitos

--- src/database/migrations/20200513004533-create-users.js
+++ src/database/migrations/20200513004533-create-users.js
@@ -6,7 +6,26 @@ module.exports = {
       { id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-      }});    
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });    
   },
 
   down: (queryInterface, Sequelize) => {   
